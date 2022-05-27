22c598847884dcc54477a503a146f75e0c29b0fc
Yuanjie Wu <yuanjiewu925@gmail.com>
2022-05-27 14:39:06 +1200

    change

--- MinimalAPI_JWT_demo/Repositories/UserRepository.cs
+++ MinimalAPI_JWT_demo/Repositories/UserRepository.cs
@@ -7,7 +7,7 @@ namespace MinimalAPI_JWT_demo.Repositories
         public static List<User> Users = new()
         {
             new() {UserName = "yuanjie_admin", EmailAddress = "yuanjie.admin@email.com", Password = "Easy_passw0rd",
-                   FirstName = "Yuanjie", LastName = "Wu", Role = "Administrator" },
+                   FirstName = "Jason", LastName = "Wu", Role = "Administrator" },
             new() {UserName = "lindsay_standard", EmailAddress = "lindsay.standard@email.com", Password = "Easy_passw0rd",
                    FirstName = "Lindsay", LastName = "Xue", Role = "Standard" },
         };
